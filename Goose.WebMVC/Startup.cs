a8ca385f0ded58a2df704874089490b19a9514cc
Louis Milrod <int4rweb@gmail.com>
2022-02-15 10:31:39 -0500

    Add project files.

---
+++ Goose.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Goose.WebMVC.Startup))]
+namespace Goose.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
