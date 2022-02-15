20c336e908171b4b759cd5bcd26669b52ef952e6
Louis Milrod <int4rweb@gmail.com>
2022-02-15 13:33:44 -0500

    created song list item

---
+++ Goose.Models/Song_Models/SongListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Models.Song_Models
+{
+    public class SongListItem
+    {
+        public int SongId { get; set; }        
+        public string Title { get; set; }       
+        public string Artist { get; set; }        
+        public string OriginalArtist { get; set; }
+    }
+}
