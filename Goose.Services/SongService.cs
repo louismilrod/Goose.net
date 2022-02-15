48414413ab2908bd58d4b2ad60a2211870eb19b9
Louis Milrod <int4rweb@gmail.com>
2022-02-15 13:34:06 -0500

    created song services

---
+++ Goose.Services/SongService.cs
@@ -0,0 +1,56 @@
+﻿using Goose.Data;
+using Goose.Models.Song_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Services
+{
+    public class SongService
+    {
+        private readonly Guid _userId;
+
+        public SongService()
+        {
+
+        }
+        public SongService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<SongListItem> GetSongLists()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Songs.Select(s => new SongListItem
+                {
+                    SongId = s.SongId,
+                    Artist = s.Artist,
+                    OriginalArtist = s.OriginalArtist,
+                    Title = s.Title,                    
+                });
+
+                return query.ToArray();
+            }
+        }
+
+        public bool CreateSong(SongCreate model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var song = new Song()
+                {
+                    Title = model.Title,
+                    Artist = model.Artist,
+                    OriginalArtist = model.OriginalArtist
+                };
+
+                ctx.Songs.Add(song);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
