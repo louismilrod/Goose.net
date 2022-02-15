a30ab738cf97a0aae0a3cf1fb6c5806b4ef03b63
Louis Milrod <int4rweb@gmail.com>
2022-02-15 11:05:15 -0500

    created song.cs filed

---
+++ Goose.Data/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Data
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Artist { get; set; }
+        public string Lyrics { get; set; }
+        public string OriginalArtist { get; set; }
+
+    }
+}
