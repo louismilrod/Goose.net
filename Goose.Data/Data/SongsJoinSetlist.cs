f1aa600b0a9ba9364dcae918fb85ce36f4cb43e7
Louis Milrod <int4rweb@gmail.com>
2022-02-15 15:36:40 -0500

    created songs joining setlist table

---
+++ Goose.Data/Data/SongsJoinSetlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Data.Data
+{
+    public class SongsJoinSetlist
+    {
+        [Key]
+        public int SongsJoinSetlistId { get; set; }
+        [Required]
+        public int PositionInSet { get; set; }
+        [Required]
+        public ICollection<Song> Song { get; set; }
+        [Required]
+        public ICollection<Setlist> Setlist { get; set; }
+    }
+}
