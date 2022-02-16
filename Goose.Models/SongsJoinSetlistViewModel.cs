782e23f56dc014af133307a29cc584226aaa836e
Louis Milrod <int4rweb@gmail.com>
2022-02-16 13:22:01 -0500

    added songjoinsetlistviewmodel

---
+++ Goose.Models/SongsJoinSetlistViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Models
+{
+    public class SongsJoinSetlistViewModel
+    {
+        public int SongsJoinSetlistId { get; set; }
+        public int SongId { get; set; }
+        public int SetlistId { get; set; }
+        public int PositionInSet { get; set; }
+    }
+}
