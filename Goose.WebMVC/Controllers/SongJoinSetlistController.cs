8fa233c782b1699053c6ea5823dc110a7cba0f70
Louis Milrod <int4rweb@gmail.com>
2022-02-16 14:03:14 -0500

    created songjoingsetlistcontroller and renamed songjoinsetlistservice

---
+++ Goose.WebMVC/Controllers/SongJoinSetlistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Goose.WebMVC.Controllers
+{
+    public class SongJoinSetlistViewModelServiceController : Controller
+    {
+        // GET: SongJoinSetlistViewModelService
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
