a84adfb7c37f63e25eedc13f838620e8b5ed5b17
Louis Milrod <int4rweb@gmail.com>
2022-02-16 09:56:13 -0500

    created setlist view model

---
+++ Goose.Models/Setlist_Models/SetlistViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Goose.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Goose.Data.Data.Setlist;
+
+namespace Goose.Models.Setlist_Models
+{
+    public class SetlistViewModel
+    {
+        public int SetlistId { get; set; }
+        public SetType SetType {get; set;}        
+        public virtual ICollection<SongsJoinSetlist> SongsForSetList { get; set; }
+        public DateTime DateofPerformance { get; set; }
+
+    }
+}
