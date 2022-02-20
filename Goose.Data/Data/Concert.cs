da1dbcd184e032e9c1172c785c4baf7772bf632a
Louis Milrod <int4rweb@gmail.com>
2022-02-20 14:57:21 -0500

    removed set variables and added an ICollection of Setlist

--- Goose.Data/Data/Concert.cs
+++ Goose.Data/Data/Concert.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,23 +15,13 @@ namespace Goose.Data.Data
         [Required]
         public string BandName { get; set; }
         [Required]
-        public int Set1_Id { get; set; }
-        public int? Set2_Id { get; set; }
-        public int? Set3_Id { get; set; }
-        public int? Encore_Id { get; set; }
-        public string Notes { get; set; }
-        [Required]
         public string Location { get; set; }
-        [Required]
-        public DateTime PerformanceDate { get; set; }        
         public string VenueName { get; set; }
 
-        public virtual Setlist Set_1 { get; set; }
-        public virtual Setlist Set_2 { get; set; }
-        public virtual Setlist Set_3 { get; set; }
-        public virtual Setlist Encore { get; set; }
-
-        //public bool InAttendence { get; set; }
-
+        [Required]
+        public DateTime PerformanceDate { get; set; }        
+        public string Notes { get; set; }     
+        public ICollection<Setlist> Setlists { get; set; }
+        
     }
 }
