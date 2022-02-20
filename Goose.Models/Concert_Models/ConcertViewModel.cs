8b52ac141173d87e2947478bd7ff62a2360488e3
Louis Milrod <int4rweb@gmail.com>
2022-02-20 14:58:31 -0500

    added concert view models

---
+++ Goose.Models/Concert_Models/ConcertViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Goose.Data;
+using Goose.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Models.Concert_Models
+{
+    public class ConcertViewModel
+    {
+        [Required]
+        public int ConcertId { get; set; }
+        [Required]
+        public string BandName { get; set; }
+        
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public DateTime DateOfPerformance { get; set; }
+        //public int? Set2_Id { get; set; }
+        //public int? Set3_Id { get; set; }
+        //public int? Encore_Id { get; set; }
+        public string Notes { get; set; }
+        public string VenueName { get; set; }
+
+        public List<Setlist> Setlists { get; set; }
+    }
+}
