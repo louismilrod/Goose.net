e9a81e26812c1eb5cf4afa16d86c90eb3194dd88
Louis Milrod <int4rweb@gmail.com>
2022-02-21 16:49:38 -0500

    created songextradetails

---
+++ Goose.Models/Song_Models/SongExtraDetails.cs
@@ -0,0 +1,21 @@
+﻿using Goose.Data;
+using Goose.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Goose.Data.Data.Setlist;
+
+namespace Goose.Models.Song_Models
+{
+    public class SongExtraDetails
+    {
+        public DateTime DateOfPerformance { get; set; }
+        public string Venue { get; set; }
+        public int Gap { get; set; }
+        public SetType Set { get; set; }
+        public Song SongBefore { get; set; }
+        public Song SongAfter { get; set; }
+    }
+}
