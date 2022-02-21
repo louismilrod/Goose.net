637c983450b2bdc63e4a5a478fea4281220f5efb
Louis Milrod <int4rweb@gmail.com>
2022-02-21 16:48:28 -0500

    ranamed ConcertSetlistListItem to ConcertListItem

--- Goose.Models/Concert_Models/ConcertListItem.cs
+++ Goose.Models/Concert_Models/ConcertListItem.cs
@@ -8,15 +8,22 @@ using System.Threading.Tasks;
 
 namespace Goose.Models.Concert_Models
 {
-    public class ConcertSetlistListItem
+    public class ConcertListItem
     {
         public int ConcertId { get; set; }
-        public List<Setlist> Sets { get; set; }        
+        public List<Setlist> Sets { get; set; }         
+        //public Setlist Set2 { get; set; }         
+        //public Setlist Set3 { get; set; }         
+        //public Setlist Encore { get; set; }         
         public string Location { get; set; }
         public string BandName { get; set; }
         public string VenueName { get; set; }
         public DateTime DateOfPerformance { get; set; }
         public string Notes { get; set; }
+
+        public List<Song> Set1 { get; set; }
+        //add a list of songs and pass into the controller
+
 
     }
 }
