43dae097542c2b1ec3afd5c3148b7323baf6fe80
Louis Milrod <int4rweb@gmail.com>
2022-02-21 10:05:14 -0500

    working on linq statement to show setlist in concert detail

--- Goose.Services/ConcertServices.cs
+++ Goose.Services/ConcertServices.cs
@@ -35,7 +35,8 @@ namespace Goose.Services
                     DateOfPerformance = s.PerformanceDate,
                     VenueName = s.VenueName,
                     Location = s.Location,
-                    Sets = s.Setlists.Select(g=>g).ToList()
+                    Sets = s.Setlists.Select(g=>g).ToList()  //maybe still need set one, set two
+
                     //Set_Two = s.Set_2.SongsForSetList.Select(g=>g.Song).ToList(),
                     //Set_Three = s.Set_3.SongsForSetList.Select(g=>g.Song).ToList(),
                     //Encore = s.Encore.SongsForSetList.Select(g=>g.Song).ToList(),
@@ -63,6 +64,55 @@ namespace Goose.Services
                 ctx.Concerts.Add(concert);
                 return ctx.SaveChanges() == 1;
             }
+        }
+
+        public ConcertViewModel GetConcertById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Concerts.Single(c=>c.ConcertId == id);
+
+                return new ConcertViewModel
+                {
+                    ConcertId =entity.ConcertId,
+                    BandName=entity.BandName,
+                    Location = entity.Location,
+                    DateOfPerformance = entity.PerformanceDate,
+                    Notes = entity.Notes,
+                    VenueName=entity.VenueName,
+                    //Setlists = entity.Setlists.Select(g=>g).ToList(), // <--- does not work
+                };
+            }
+        }
+
+        public bool UpdateConcert(ConcertViewModel model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Concerts.Single(c => c.ConcertId == model.ConcertId);
+
+                    entity.BandName = model.BandName;
+                    entity.PerformanceDate = model.DateOfPerformance;
+                    entity.VenueName = model.VenueName;
+                    entity.Location = model.Location;
+                    entity.Notes = model.Notes;
+
+                return ctx.SaveChanges() == 1;
+
+            }
+        }
+
+        public bool DeleteConcert(int concertId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Concerts.Single(c => c.ConcertId == concertId);
+                ctx.Concerts.Remove(entity);
+                return ctx.SaveChanges() == 1;
+
+            }
+
+
         }
     }
 }
