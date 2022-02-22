9c60cb88b4517830a30e23a65ee08d3615243866
Louis Milrod <int4rweb@gmail.com>
2022-02-22 10:39:33 -0500

    edited out list<setlist>> list<song>

--- Goose.Models/Concert_Models/ConcertListItem.cs
+++ Goose.Models/Concert_Models/ConcertListItem.cs
@@ -1,5 +1,6 @@
 ﻿using Goose.Data;
 using Goose.Data.Data;
+using Goose.Models.Setlist_Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace Goose.Models.Concert_Models
     public class ConcertListItem
     {
         public int ConcertId { get; set; }
-        public List<Setlist> Sets { get; set; }         
+       // public List<Setlist> Sets { get; set; }         
         //public Setlist Set2 { get; set; }         
         //public Setlist Set3 { get; set; }         
         //public Setlist Encore { get; set; }         
@@ -20,8 +21,9 @@ namespace Goose.Models.Concert_Models
         public string VenueName { get; set; }
         public DateTime DateOfPerformance { get; set; }
         public string Notes { get; set; }
+        public List<SetlistDataForConcertDetailView> Setlists { get; set; }
 
-        public List<Song> Set1 { get; set; }
+        //public List<Song> Set1 { get; set; }
         //add a list of songs and pass into the controller
 
 
