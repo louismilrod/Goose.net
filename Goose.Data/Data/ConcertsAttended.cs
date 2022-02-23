9b1db152ad49b54be63f0267001c4c53015569f6
Louis Milrod <int4rweb@gmail.com>
2022-02-23 16:49:33 -0500

    creation

---
+++ Goose.Data/Data/ConcertsAttended.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Data.Data
+{
+    public class ConcertsAttended
+    {
+        [Key]
+        public int ConcertsAttendedId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public int ConcertId { get; set; }
+        public virtual Concert Concert { get; set; }
+    }
+}
---
+++ Goose.Data/Migrations/202202232128535_ConcertsAttended.cs
@@ -0,0 +1,31 @@
+﻿namespace Goose.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ConcertsAttended : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ConcertsAttended",
+                c => new
+                    {
+                        ConcertsAttendedId = c.Int(nullable: false, identity: true),
+                        UserId = c.Guid(nullable: false),
+                        ConcertId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ConcertsAttendedId)
+                .ForeignKey("dbo.Concert", t => t.ConcertId, cascadeDelete: true)
+                .Index(t => t.ConcertId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ConcertsAttended", "ConcertId", "dbo.Concert");
+            DropIndex("dbo.ConcertsAttended", new[] { "ConcertId" });
+            DropTable("dbo.ConcertsAttended");
+        }
+    }
+}
