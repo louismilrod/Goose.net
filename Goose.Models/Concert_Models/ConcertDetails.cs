4610c7c660425794180f583e014124c82dbd3638
Louis Milrod <int4rweb@gmail.com>
2022-02-23 00:55:13 -0500

    created a concert details file

---
+++ Goose.Models/Concert_Models/ConcertDetails.cs
@@ -0,0 +1,35 @@
+﻿using Goose.Models.Setlist_Models;
+using Goose.Models.Song_Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Goose.Data.Data.Setlist;
+
+namespace Goose.Models.Concert_Models
+{
+    public class ConcertDetails
+    { 
+        public int ConcertId { get; set; }
+        public string BandName { get; set; }
+        public string Location { get; set; }
+        public DateTime DateOfPerformance { get; set; }
+        public string Notes { get; set; }
+        public string VenueName { get; set; }
+        public List<SetlistDataForConcertDetailView> Setlists { get; set; }
+     
+    }
+
+    public class SetlistDataForConcertDetailView
+    {
+        public int SetlistId { get; set; }
+        [Display(Name = "Set Number")]
+        public SetType SetNumber { get; set; }
+
+        [Display(Name = "Setlist")]
+        public List<SongDetail> SongsForSetlist { get; set; }
+        
+    }
+}
