9ca97cbb91ac897f88408719e075da0a952b8ee2
Louis Milrod <int4rweb@gmail.com>
2022-02-23 16:49:46 -0500

    creation

---
+++ Goose.Models/ConcertsAttendedModel.cs
@@ -0,0 +1,17 @@
+﻿using Goose.Models.Concert_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goose.Models
+{
+    public class ConcertsAttendedModel
+    {
+        public Guid UserId { get; set; }
+        public int ConcertId { get; set; }
+        public bool Attendance { get; set; }
+        public List<ConcertListItem> ConcertListItems { get; set; }
+    }
+}
