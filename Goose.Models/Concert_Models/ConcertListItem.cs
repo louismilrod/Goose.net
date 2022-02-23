c29f661e08defa12bc115c17b83db9897061f759
Louis Milrod <int4rweb@gmail.com>
2022-02-23 00:55:28 -0500

    removed notes

--- Goose.Models/Concert_Models/ConcertListItem.cs
+++ Goose.Models/Concert_Models/ConcertListItem.cs
@@ -11,21 +11,12 @@ namespace Goose.Models.Concert_Models
 {
     public class ConcertListItem
     {
-        public int ConcertId { get; set; }
-       // public List<Setlist> Sets { get; set; }         
-        //public Setlist Set2 { get; set; }         
-        //public Setlist Set3 { get; set; }         
-        //public Setlist Encore { get; set; }         
+        public int ConcertId { get; set; }            
         public string Location { get; set; }
         public string BandName { get; set; }
         public string VenueName { get; set; }
         public DateTime DateOfPerformance { get; set; }
         public string Notes { get; set; }
         public List<SetlistDataForConcertDetailView> Setlists { get; set; }
-
-        //public List<Song> Set1 { get; set; }
-        //add a list of songs and pass into the controller
-
-
     }
 }
