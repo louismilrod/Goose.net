173c5dca3f31d82fa4da5b550b7e858135930e6e
Louis Milrod <int4rweb@gmail.com>
2022-02-25 09:53:28 -0500

    migrations and project files

---
+++ Goose.Data/Migrations/202202250005023_RemovedBoolFromInAttenDance.cs
@@ -0,0 +1,18 @@
+﻿namespace Goose.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedBoolFromInAttenDance : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.ConcertsAttended", "InAttendance");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ConcertsAttended", "InAttendance", c => c.Boolean(nullable: false));
+        }
+    }
+}
