b1e927b7b3c1f15589d268dde3b058a30432b95a
Louis Milrod <int4rweb@gmail.com>
2022-02-25 09:55:56 -0500

    modified concerts attended to contain more information

--- Goose.Models/ConcertsAttendedModel.cs
+++ Goose.Models/ConcertsAttendedModel.cs
@@ -1,6 +1,7 @@
 ﻿using Goose.Models.Concert_Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,16 @@ namespace Goose.Models
 {
     public class ConcertsAttendedModel
     {
+        [Required]
         public Guid UserId { get; set; }
+        [Required]
         public int ConcertId { get; set; }
-        public bool Attendance { get; set; }
-        public List<ConcertListItem> ConcertListItems { get; set; }
-    }
+        public string Location { get; set; }
+        public string BandName { get; set; }
+        public string VenueName { get; set; }
+        public DateTime DateOfPerformance { get; set; }
+        public string Notes { get; set; }
+        public List<SetlistDataForConcertDetailView> Setlists { get; set; }
+        public bool InAttendance { get; set; }        
+    }  
 }
