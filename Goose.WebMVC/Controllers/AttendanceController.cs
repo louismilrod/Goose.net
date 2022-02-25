730f3b6873b00e91a3a260b68a685c76f93745df
Louis Milrod <int4rweb@gmail.com>
2022-02-25 09:59:59 -0500

    creation!

---
+++ Goose.WebMVC/Controllers/AttendanceController.cs
@@ -0,0 +1,28 @@
+﻿using Goose.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Goose.WebMVC.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        // GET: Attendance
+        public ActionResult Index()
+        {
+            var service = ConcertsAttendedServices();
+            var model = service.GetConcertsAttended();
+            return View(model);
+        }
+
+        private ConcertsAttendedServices ConcertsAttendedServices()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new ConcertsAttendedServices(userId);
+            return service;
+        }
+    }
+}
