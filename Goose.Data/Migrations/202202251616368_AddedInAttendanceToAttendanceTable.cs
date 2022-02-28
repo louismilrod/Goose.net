096e9db18c9869b09aa62129003428d7df5ba45c
Louis Milrod <int4rweb@gmail.com>
2022-02-28 09:48:42 -0500

    migrations and data

---
+++ Goose.Data/Migrations/202202251616368_AddedInAttendanceToAttendanceTable.cs
@@ -0,0 +1,18 @@
+﻿namespace Goose.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedInAttendanceToAttendanceTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ConcertsAttended", "InAttendance", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ConcertsAttended", "InAttendance");
+        }
+    }
+}
