fa85d2b553b61eb4af1639ea11dae7c89e18f822
Louis Milrod <int4rweb@gmail.com>
2022-02-28 13:10:30 -0500

    added unattend to attendance controller

--- Goose.WebMVC/Controllers/AttendanceController.cs
+++ Goose.WebMVC/Controllers/AttendanceController.cs
@@ -18,10 +18,30 @@ namespace Goose.WebMVC.Controllers
             return View(model);
         }
 
+        public ActionResult Unattend(int id)
+        {
+            var service = CreateConcertService();
+
+            if (service.Wait_Did_I_Go_To_That(id) == false)
+            {
+                TempData["SaveResult"] = "I knew you didn't go";
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
         private ConcertsAttendedServices ConcertsAttendedServices()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new ConcertsAttendedServices(userId);
+            return service;
+        }
+
+        private ConcertServices CreateConcertService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new ConcertServices(userId);
             return service;
         }
     }
