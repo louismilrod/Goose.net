2a8d7139923a6275fe3ff7b4edc5a2410ed64ac5
Louis Milrod <int4rweb@gmail.com>
2022-03-01 11:24:55 -0500

    created select list item for setlist create

--- Goose.Models/Setlist_Models/SetlistCreate.cs
+++ Goose.Models/Setlist_Models/SetlistCreate.cs
@@ -4,14 +4,19 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using static Goose.Data.Data.Setlist;
 
 namespace Goose.Models.Setlist_Modles
 {
     public class SetlistCreate
     {
+        [Display(Name = "Concert")]
         public int ConcertId { get; set; }
+        public SelectList SelectListSetlist { get; set; }
+
         [Display(Name = "Set Number")]
         public SetType SetNumber { get; set; }
+
     }
 }
